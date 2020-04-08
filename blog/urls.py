afe22217397dac69c2c41355c7db01a4a9cbb785
eornr <eornrsayclub@gmail.com>
2020-04-08 13:00:07 +0900

    add COVID 19 Korea map data with url pattern

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('COVID19Korea/', views.COVID19_map_data, name='COVID19_map_data'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def COVID19_map_data(request):
+    return render(request, 'blog/COVID19/COVID_19_in_South_Korea_200317.html', {})
