68c1137ddd0a6e51d661c0a724fafa700dc4a7f2
eornr <eornrsayclub@gmail.com>
2020-04-17 17:03:56 +0900

    Modified templates to display posts from database

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def COVID19_map_data(request):
     return render(request, 'blog/COVID19/COVID_19_in_South_Korea_200317.html', {})
